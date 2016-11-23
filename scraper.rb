c06ca087d374c3b6cfa6f8192400412225173efb
Tony Bowden <tony@tmtm.com>
2016-11-23 09:44:59 +0000

    Archive some other (unprocessed) pages
    The only two relevant pages I could find from the outgoing legislature
    were the Commissions and the list of female deputies.

--- scraper.rb
+++ scraper.rb
@@ -43,3 +43,7 @@ term = {
 ScraperWiki.save_sqlite([:id], term, 'terms')
 
 scrape_list('http://www.assemblee-nationale.ga/object.getObject.do?id=190')
+
+# archive some pages for later processing
+open('http://www.assemblee-nationale.ga/34-deputes/168-bureaux-des-commissions/')
+open('http://www.assemblee-nationale.ga/34-deputes/153-les-femmes-deputes/')
